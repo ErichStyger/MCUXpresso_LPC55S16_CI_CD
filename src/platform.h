95b9e8ee854541ed3ec40c3aeeb0598aa2dd08f5
Erich Styger <erich.styger@hslu.ch>
2024-10-20 11:47:58 +0200

    using LinkServer test runner

--- src/platform.h
+++ src/platform.h
@@ -16,13 +16,13 @@
 #endif
 
 /* select only one runner: update the tests/CMakeLists.txt as well! */
-#define PL_CONFIG_USE_RUNNER_JLINK        (1)
-#define PL_CONFIG_USE_RUNNER_LINKSERVER   (0)
+#define PL_CONFIG_USE_RUNNER_JLINK        (0)
+#define PL_CONFIG_USE_RUNNER_LINKSERVER   (1)
 
 /* select only one way to report: */
 #define PL_CONFIG_USE_RTT                 (1 && PL_CONFIG_USE_RUNNER_JLINK)        /* J-Link only: using J-Run and SEGGER RTT for log and test output */
-#define PL_CONFIG_USE_SEMIHOSTING         (0 && PL_CONFIG_USE_RUNNER_LINKSERVER)   /* LinkServer only: using semihosting for log and test output */
-#define PL_CONFIG_USE_SHELL_UART          (1 && PL_CONFIG_USE_RUNNER_LINKSERVER)   /* LinkServer only: using UART for log and test output */
+#define PL_CONFIG_USE_SEMIHOSTING         (1 && PL_CONFIG_USE_RUNNER_LINKSERVER)   /* LinkServer only: using semihosting for log and test output */
+#define PL_CONFIG_USE_SHELL_UART          (0 && PL_CONFIG_USE_RUNNER_LINKSERVER)   /* LinkServer only: using UART for log and test output */
 
 #define PL_CONFIG_USE_EXPERIMENTAL        (1 && PL_CONFIG_USE_RUNNER_LINKSERVER)    /* experimental feature with arguments */
 
