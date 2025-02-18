09fb391a96902c22e320f0838ee766c5b586c44d
Erich Styger <erich.styger@hslu.ch>
2025-02-18 08:54:23 +0100

    added flashing task

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include "platform.h"
 #include "application.h"
 
+#include "McuArmTools.h"
+int32_t val, val2;
 
 int main(void) {
   BOARD_InitBootPins();
@@ -21,6 +23,8 @@ int main(void) {
    /* set BOD VBAT level to 1.65V, otherwise higher clock speeds might cause a brown-out! */
   POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
 
+  val  = McuArmTools_GetUnusedMainStackSpace();
+  val2 = McuArmTools_GetLinkerMainStackSize();
   APP_Run();
   while(1) {
     __asm volatile ("nop");
